7f1f16e31740569bf4c83ac1a8de171e37ab6b8b
MartinPaulo <martin.paulo@gmail.com>
2025-02-27 12:49:28 +1100

    Converted Java to Kotlin

--- app/src/androidTest/java/nz/co/martinpaulo/wordjambalaya/DictionaryTest.kt
+++ app/src/androidTest/java/nz/co/martinpaulo/wordjambalaya/DictionaryTest.kt
@@ -1,76 +1,111 @@
-package nz.co.martinpaulo.wordjambalaya;
+package nz.co.martinpaulo.wordjambalaya
 
-import android.test.AndroidTestCase;
+import junit.framework.TestCase
+import nz.co.martinpaulo.wordjambalaya.Dictionary.Companion.buildDictionary
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+class DictionaryTest: TestCase() {
+    private var dictionary: Dictionary? = null
+    private var columns: MutableList<List<String>>? = null
 
-public class DictionaryTest extends AndroidTestCase {
-
-    private Dictionary dictionary;
-    private List<List<String>> columns;
-
-    public void setUp() throws Exception {
-        super.setUp();
-        dictionary = Dictionary.buildDictionary(getContext());
-        columns = new ArrayList<List<String>>();
+    @Throws(Exception::class)
+    override fun setUp() {
+        dictionary = buildDictionary(androidx.test.core.app.ApplicationProvider.getApplicationContext())
+        columns = ArrayList()
     }
 
-    public void tearDown() throws Exception {
-        super.tearDown();
-    }
-
-
-    public void testOrderCharacters() {
+    fun testOrderCharacters() {
         // shouldn't have build entire dictionary to test this...
         // but we need to know if it works, as we are using it in our tests.
-        assertEquals("abeggr", dictionary.orderCharacters("beggar"));
+        assertEquals("abeggr", dictionary!!.orderCharacters("beggar"))
     }
 
-    public void testOrderCharactersOfWords() {
+    fun testOrderCharactersOfWords() {
         // shouldn't have build entire dictionary to test this...
         // but we need to know if it works, as we are using it in our tests.
-        assertEquals("abeeggmnory", dictionary.orderCharactersOf(Arrays.asList("beggar", "money")));
+        assertEquals("abeeggmnory",
+            dictionary!!.orderCharactersOf(mutableListOf("beggar", "money")))
     }
 
-    public void testGetTwoCandidateWordSets() throws Exception {
-        String[] words = {"beggar", "thief"};
-        assertDictionaryFindsCandidateWordSets(words);
+    @Throws(Exception::class)
+    fun testGetTwoCandidateWordSets() {
+        val words = arrayOf("beggar", "thief")
+        assertDictionaryFindsCandidateWordSets(words)
     }
 
-    public void testGetThreeCandidateWordSets() throws Exception {
-        String[] words = {"bag", "of", "bones"};
-        assertDictionaryFindsCandidateWordSets(words);
+    @Throws(Exception::class)
+    fun testGetThreeCandidateWordSets() {
+        val words = arrayOf("bag", "of", "bones")
+        assertDictionaryFindsCandidateWordSets(words)
     }
 
-    public void testGetFourCandidateWordSets() throws Exception {
-        String[] words = {"broken", "down", "relics", "run"};
-        assertDictionaryFindsCandidateWordSets(words);
+    @Throws(Exception::class)
+    fun testGetFourCandidateWordSets() {
+        val words = arrayOf("broken", "down", "relics", "run")
+        assertDictionaryFindsCandidateWordSets(words)
     }
 
-    private void assertDictionaryFindsCandidateWordSets(String[] words) {
-        addWordsToColumns(words);
-        String muddledWordsWithOrderedCharacters = dictionary.orderCharactersOf(Arrays.asList(words));
-        List<List<String>> candidateAnswerSets = dictionary.getCandidateWordSets(muddledWordsWithOrderedCharacters, columns);
-        assertEquals(getAnswer(words), candidateAnswerSets);
-    }
-
-    private void addWordsToColumns(String[] words) {
-        for (String word : words) {
-            columns.add(getColumn(word));
+    private fun addWordsToColumns(words: Array<String>) {
+        for (word in words) {
+            columns!!.add(getColumn(word))
         }
     }
 
-    private List<List<String>> getAnswer(String... words) {
-        List<List<String>> result = new ArrayList<List<String>>();
-        result.add(getColumn(words));
-        return result;
+    private fun getAnswer(vararg words: String): List<List<String>> {
+        val result: MutableList<List<String>> = ArrayList()
+        result.add(getColumn(*words))
+        return result
     }
 
-    private List<String> getColumn(String... words) {
-        List<String> result = new ArrayList<String>();
-        result.addAll(Arrays.asList(words));
-        return result;
+    private fun getColumn(vararg words: String): List<String> {
+        val result: MutableList<String> = ArrayList()
+        result.addAll(listOf(*words))
+        return result
+    }
+
+    private fun assertDictionaryFindsCandidateWordSets(words: Array<String>) {
+        addWordsToColumns(words)
+        val muddledWordsWithOrderedCharacters =
+            dictionary!!.orderCharactersOf(listOf(*words))
+        val candidateAnswerSets = dictionary!!.getCandidateWordSets(
+            muddledWordsWithOrderedCharacters,
+            columns!!
+        )
+        assertEquals(getAnswer(*words), candidateAnswerSets)
+    }
+
+    
+    fun findJumbledWords() {
+    }
+
+    
+    fun orderCharactersOf() {
+    }
+
+    
+    fun getCandidateWordSets() {
+    }
+
+    
+    fun findPossibleAnswers() {
+    }
+
+    
+    fun addAnswerChar() {
+    }
+
+    
+    fun removeAnswerChar() {
+    }
+
+    
+    fun getAnswerLength() {
+    }
+
+    
+    fun buildDictionary() {
+    }
+
+    
+    fun getInstance() {
     }
 }
